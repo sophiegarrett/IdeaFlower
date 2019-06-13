40a81ae33e915df5b60b7a30f461de59bc1bdbe9
Sophie Garrett <sarasnake19@gmail.com>
2019-06-12 21:37:24 -0400

    add other pages

--- src/firebase/functions/index.js
+++ src/firebase/functions/index.js
@@ -4,7 +4,15 @@ const app = express();
 const path = require('path');
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname, 'express', '/index.html'));
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'express', '/404.html'));
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'express', '/login.html'));
 });
 
 exports.app = functions.https.onRequest(app);
