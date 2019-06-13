22069f2bd1865fe69a2774451e07fca44d704d46
Sophie Garrett <sarasnake19@gmail.com>
2019-06-12 21:30:10 -0400

    add main page

--- src/firebase/functions/index.js
+++ src/firebase/functions/index.js
@@ -1,29 +1,10 @@
 const functions = require('firebase-functions');
 const express = require('express');
 const app = express();
+const path = require('path');
 
-app.get('/', (req, res) => {
-  const date = new Date();
-  const hours = (date.getHours() % 12) + 1;  // London is UTC + 1hr;
-  res.send(`
-    <!doctype html>
-    <head>
-      <title>Time</title>
-      <link rel="stylesheet" href="/style.css">
-      <script src="/script.js"></script>
-    </head>
-    <body>
-      <p>In London, the clock strikes:
-        <span id="bongs">${'BONG '.repeat(hours)}</span></p>
-      <button onClick="refresh(this)">Refresh</button>
-    </body>
-  </html>`);
-});
-
-app.get('/api', (req, res) => {
-  const date = new Date();
-  const hours = (date.getHours() % 12) + 1;  // London is UTC + 1hr;
-  res.json({bongs: 'BONG '.repeat(hours)});
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 exports.app = functions.https.onRequest(app);
