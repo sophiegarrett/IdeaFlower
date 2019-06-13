327f6ea503f3ee10f9081037c86ea05a095c8205
Sophie Garrett <sarasnake19@gmail.com>
2019-06-12 21:52:07 -0400

    add 404 page

--- src/firebase/functions/index.js
+++ src/firebase/functions/index.js
@@ -1,18 +1,22 @@
 const functions = require('firebase-functions');
+
 const express = require('express');
 const app = express();
 const path = require('path');
+const router = express.Router();
 
-app.get('/', function(req, res) {
+router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'express', '/index.html'));
 });
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'express', '/404.html'));
+router.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'express', '/login.html'));
 });
 
-app.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname, 'express', '/login.html'));
+app.use('/', router);
+
+app.use ((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'express', '/404.html'));
 });
 
 exports.app = functions.https.onRequest(app);
