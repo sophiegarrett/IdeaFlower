e05b6bbe4d0d7a00d9fe095805f17f0fc02492b3
Sophie Garrett <sarasnake19@gmail.com>
2019-06-17 21:35:46 -0400

    add function to create user profiles

--- src/firebase/functions/index.js
+++ src/firebase/functions/index.js
@@ -2,7 +2,9 @@
 
 const functions = require('firebase-functions');
 const app = require('./express/app');
+const createProfile = require('./createProfile');
 
 module.exports = {
   'app': functions.https.onRequest(app),
+  authOnCreate: functions.auth.user().onCreate(createProfile),
 };
