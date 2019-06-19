750f262abc82a9437300ee57999bb9e874d4cb47
Sophie Garrett <sarasnake19@gmail.com>
2019-06-19 04:18:33 -0400

    copy listAllIdeas to listUserIdeas

--- src/firebase/functions/express/public/javascript/list_ideas.js
+++ src/firebase/functions/express/public/javascript/list_ideas.js
@@ -28,14 +28,20 @@ listAllIdeas = function() {
 // Function that lists all ideas submitted by a specific user.
 listUserIdeas = function(uid) {
   var database = firebase.firestore();
-  var ideas = [];
+  var table = document.getElementById("idealist");
 
   db.collection("ideas").where("uid", "==", uid)
     .get()
     .then(function(querySnapshot) {
         querySnapshot.forEach(function(doc) {
-          var idea = {id: doc.id, title: doc.data().title, timestamp: doc.data().timestamp};
-          ideas.push(idea);
+          var row = table.insertRow();
+          var title_cell = row.insertCell(0);
+          var user_cell = row.insertCell(1);
+          var date_cell = row.insertCell(2);
+
+          title_cell.innerHTML = "<a href='/idea/" + doc.id + "'>" + doc.data().title + "</a>";
+          user_cell.innerHTML = doc.data().uid;
+          date_cell.innerHTML = doc.data().timestamp;
         });
         return null;
     })
