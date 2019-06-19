cff9fb8c524237656f74d62ad2fc95e8debbdc16
Sophie Garrett <sarasnake19@gmail.com>
2019-06-18 22:58:16 -0400

    add submitIdea function

--- src/firebase/functions/index.js
+++ src/firebase/functions/index.js
@@ -2,9 +2,11 @@
 
 const functions = require('firebase-functions');
 const app = require('./express/app');
+const submitIdea = require('./submitIdea');
 const createUser = require('./createUser');
 
 module.exports = {
   'app': functions.https.onRequest(app),
+  'submitIdea': functions.https.onCall(submitIdea),
   authOnCreate: functions.auth.user().onCreate(createUser),
 };
---
+++ src/firebase/functions/submitIdea.js
@@ -0,0 +1,12 @@
+// submitIdea.js
+
+const {
+  database,
+} = require('./admin');
+
+// Submits a new idea to the database.
+const submitIdea = (idea) => {
+  
+};
+
+module.exports = submitIdea;
