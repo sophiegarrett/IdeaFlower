1a8789bc17ab9a6f39888a6c3d72548703b98527
Sophie Garrett <sarasnake19@gmail.com>
2019-06-18 20:01:11 -0400

    clean up user settings code

--- src/firebase/functions/express/public/javascript/user_settings.js
+++ src/firebase/functions/express/public/javascript/user_settings.js
@@ -5,14 +5,20 @@ isEmpty = function(input) {
   return !input || !input.trim();
 }
 
-// Function that checks if a display name contains invalid characters.
-isInvalid = function(input) {
+// Function that checks if a display name is valid.
+isValidDisplayName = function(input) {
   var regexp = /^[a-zA-Z0-9-_]+$/;
   if (input.search(regexp) === -1) {
-    return true;
+    return "Error: Display names can only contain letters, numbers, and underscores.";
+  }
+  else if (input.length < 3) {
+    return "Error: Display names must be at least three characters long.";
+  }
+  else if (input.length > 25) {
+    return "Error: Display names must be no more than 25 characters long.";
   }
   else {
-    return false;
+    return "valid";
   }
 }
 
