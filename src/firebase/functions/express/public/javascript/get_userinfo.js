fd1a36a9309ec144bf62f7231f2beebb33ec8537
Sophie Garrett <sarasnake19@gmail.com>
2019-07-29 06:40:32 -0400

    bug fixes

--- src/firebase/functions/express/public/javascript/get_userinfo.js
+++ src/firebase/functions/express/public/javascript/get_userinfo.js
@@ -7,20 +7,14 @@ getUserData = function(uid) {
 
   docRef.get().then(function(doc) {
     if (doc.exists) {
-      var userData = doc.data();
+      return doc.data();
     } else {
       console.log("User does not exist.");
+      return null;
     }
   }).catch(function(error) {
     console.log("Error getting document:", error);
   });
-
-  if (userData != null) {
-    return userData;
-  }
-  else {
-    console.log("Error getting user data.");
-    return null;
   }
 }
 
@@ -28,7 +22,7 @@ getUserData = function(uid) {
 getDisplayName = function(uid) {
   var userData = getUserData(uid);
 
-  if (userData.displayName != null) {
+  if (userData.displayName !== null) {
     return userData.displayName;
   }
   else {
