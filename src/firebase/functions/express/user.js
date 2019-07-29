32141c84c54737a6d36c5405363a5c3b3c1127b9
Sophie Garrett <sarasnake19@gmail.com>
2019-07-29 06:02:49 -0400

    re-link user pages to database

--- src/firebase/functions/express/user.js
+++ src/firebase/functions/express/user.js
@@ -1,23 +1,26 @@
 // user.js
 
-const admin = require('firebase-admin');
 const {
   database,
 } = require('../admin');
 
 exports.view = function(req, res) {
   var id = req.params.id;
-  admin.auth().getUser(id)
-    .then(function(userRecord) {
-      // See the UserRecord reference doc for the contents of userRecord.
-      var user = {id: userRecord.uid, name: userRecord.displayName};
-      res.render('users/view', { title: 'Viewing user ' + user.name, user: user });
-      return null;
-    })
-    .catch(function(error) {
-      console.log('Error fetching user data:', error);
-      res.render('404', { title: 'Page Not Found' });
-    });
+  var userdoc = database.collection('users').doc(id).get()
+   .then(doc => {
+     if (!doc.exists) {
+       console.log('Cannot find user ' + id);
+       res.render('404', { title: 'Page Not Found' });
+     } else {
+       var user = {id: doc.id, name: doc.data().displayName};
+       res.render('users/view', { title: 'Viewing user ' + user.name, user: user });
+     }
+     return null;
+   })
+   .catch(function(error) {
+     console.log('Error fetching user data:', error);
+     res.render('404', { title: 'Page Not Found' });
+   });
 };
 
 exports.settings = function(req, res) {
@@ -27,12 +30,3 @@ exports.settings = function(req, res) {
 exports.welcome = function(req, res) {
   res.render('users/welcome', { title: 'Welcome' });
 }
-
-exports.update = function(req, res){
-  // Normally you would handle all kinds of
-  // validation and save back to the db
-  var user = req.body.user;
-  req.user.name = user.name;
-  req.user.email = user.email;
-  res.redirect('back');
-};
