5411808669cd84c6506dfa4868018011504004d2
Sophie Garrett <sarasnake19@gmail.com>
2019-07-29 05:20:43 -0400

    bug fixes

--- src/firebase/functions/createUser.js
+++ src/firebase/functions/createUser.js
@@ -2,17 +2,22 @@
 
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const database = require('./admin');
+
+const {
+  database,
+} = require('./admin');
 
 // Sets a newly-created user's displayName to a randomly-generated string.
 // Adds a new user record to the database.
 const createUser = (userRecord, context) => {
-  const { displayName, uid } = userRecord;
+  var { displayName, uid } = userRecord;
   var new_name = newID();
 
   admin.auth().updateUser(uid, {
     displayName: new_name
   });
+
+  displayName = new_name;
 
   return database
     .collection('users')
